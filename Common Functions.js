40f595dc806feec36d46fd504925404f905da67d
Thái <62926491+nhthai173@users.noreply.github.com>
2022-10-31 16:17:48 +0700

    Update Common Functions.js

--- Common Functions.js
+++ Common Functions.js
@@ -149,7 +149,10 @@ function smartCompare(a, b, type = ''){
       const bLength = b.length || Object.keys(b).length
       if(aIsArray && !bIsArray) return false
       if(!aIsArray && bIsArray) return false
-      if(!aLength && !bLength) return true
+      if(!aLength && !bLength){
+        if(a.toString() !== b.toString()) return false
+        return true
+      }
       if(aLength !== bLength) return false
       output = true
       for(const i in a){
@@ -204,7 +207,11 @@ function isEmptyVariable(a, {allowZero = true, allowEmtyString = false, evenStri
     return true
   }
   if(Array.isArray(a) && a.length === 0) return true
-  if(typeof a === 'object' && Object.keys(a).length === 0) return true
+  if(typeof a === 'object' && Object.keys(a).length === 0){
+    let emptyObj = {}
+    emptyObj = emptyObj.toString()
+    if(a.toString() === emptyObj) return true
+  }
   if(evenString){
     if(a === 'undefined' || a === 'null' || a === 'NaN')
       return true
