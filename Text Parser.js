809416c24b06f4325d47a2f43e00a6d3aafac6b7
Thái <62926491+nhthai173@users.noreply.github.com>
2022-11-25 22:28:48 +0700

    update toRawText

--- Text Parser.js
+++ Text Parser.js
@@ -39,6 +39,7 @@ function toRawText(str = '', {
   removeSpace = true,
   lowerCase = true
 } = {}) {
+  if (isEmptyVariable(str)) return ''
   str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
   str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
   str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
