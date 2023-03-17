8c5d213cbb411beed21cedd7892d84a1633ac6a2
Thái <62926491+nhthai173@users.noreply.github.com>
2023-03-17 19:14:23 +0700

    Add sha256

--- Hash Functions.js
+++ Hash Functions.js
@@ -48,3 +48,42 @@ function MD5(input, isShortMode) {
   return txtHash;
 
 }
+
+
+
+
+
+
+
+
+function Sha256Hash(value) {
+  return BytesToHex(
+    Utilities.computeDigest(
+      Utilities.DigestAlgorithm.SHA_256, value));
+}
+
+
+
+
+
+
+
+
+
+function BytesToHex(bytes) {
+  let hex = [];
+  for (let i = 0; i < bytes.length; i++) {
+    let b = parseInt(bytes[ i ]);
+    if (b < 0) {
+      c = (256 + b).toString(16);
+    } else {
+      c = b.toString(16);
+    }
+    if (c.length == 1) {
+      hex.push("0" + c);
+    } else {
+      hex.push(c);
+    }
+  }
+  return hex.join("");
+}
