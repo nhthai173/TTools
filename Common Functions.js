e828e4230a5244078ef99ed3a1331136640c89df
Thái <62926491+nhthai173@users.noreply.github.com>
2023-03-17 19:13:44 +0700

    Split hash functions to a new file

--- Common Functions.js
+++ Common Functions.js
@@ -10,57 +10,6 @@ function getScriptProperty(name = '') {
 
 
 
-/**
- *
- * @param {(string|Bytes[])} input The value to hash.
- * @param {boolean} isShortMode Set true for 4 digit shortened hash, else returns usual MD5 hash.
- * @return {string} The hashed input
- * @customfunction
- *
- */
-function MD5(input, isShortMode) {
-  var txtHash = '';
-  var rawHash = Utilities.computeDigest(
-    Utilities.DigestAlgorithm.MD5,
-    input);
-
-  var isShortMode = (isShortMode == true) ? true : false;
-
-  if (!isShortMode) {
-    for (i = 0; i < rawHash.length; i++) {
-
-      var hashVal = rawHash[ i ];
-
-      if (hashVal < 0) {
-        hashVal += 256;
-      };
-      if (hashVal.toString(16).length == 1) {
-        txtHash += '0';
-      };
-      txtHash += hashVal.toString(16);
-    };
-  } else {
-    for (j = 0; j < 16; j += 8) {
-
-      hashVal = (rawHash[ j ] + rawHash[ j + 1 ] + rawHash[ j + 2 ] + rawHash[ j + 3 ])
-        ^ (rawHash[ j + 4 ] + rawHash[ j + 5 ] + rawHash[ j + 6 ] + rawHash[ j + 7 ]);
-
-      if (hashVal < 0) {
-        hashVal += 1024;
-      };
-      if (hashVal.toString(36).length == 1) {
-        txtHash += "0";
-      };
-
-      txtHash += hashVal.toString(36);
-    };
-  };
-
-  // change below to "txtHash.toUpperCase()" if needed
-  return txtHash;
-
-}
-
 
 /**
  * 
